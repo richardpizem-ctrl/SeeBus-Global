2884e3a7b9717cc7a428de1cf2bca2c5e8de032a
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-18 04:59:36 +0200

    Create voice_service.py

---
+++ SeeBus-Global-main/backend/services/voice_service.py
@@ -0,0 +1,17 @@
+class VoiceService:
+
+    def generate_announcement(self, state, route_short_name, stop_name):
+        """
+        Vytvorí text hlásenia podľa stavu.
+        """
+
+        if state == "ARRIVING":
+            return f"Linka {route_short_name} prichádza na zastávku {stop_name}."
+
+        if state == "AT_STOP":
+            return f"Linka {route_short_name} stojí na zastávke {stop_name}."
+
+        if state == "DEPARTING":
+            return f"Linka {route_short_name} odchádza zo zastávky {stop_name}."
+
+        return None
