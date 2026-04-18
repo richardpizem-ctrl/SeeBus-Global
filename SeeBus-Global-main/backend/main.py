811ba49805f27e7dc678e0ddb0d64268803f4d4c
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-18 05:32:21 +0200

    Update main.py

--- SeeBus-Global-main/backend/main.py
+++ SeeBus-Global-main/backend/main.py
@@ -1,9 +1,21 @@
-
 from fastapi import FastAPI
+
+# Status endpoint
 from backend.api.routes import get_status
 
+# API endpointy
+from backend.api.events_endpoint import router as events_router
+from backend.api.stream_endpoint import router as stream_router
+
+
 app = FastAPI()
+
 
 @app.get("/status")
 def status():
     return get_status()
+
+
+# Registrácia API routerov
+app.include_router(events_router)
+app.include_router(stream_router)
