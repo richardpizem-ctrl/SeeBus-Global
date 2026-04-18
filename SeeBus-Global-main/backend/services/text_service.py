16c22242fa961a72144492c4d091ea6eae461f48
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-18 05:01:19 +0200

    Create text_service.py

---
+++ SeeBus-Global-main/backend/services/text_service.py
@@ -0,0 +1,17 @@
+class TextService:
+
+    def generate_text_message(self, state, route_short_name, stop_name):
+        """
+        Vytvorí textové hlásenie pre nepočujúcich.
+        """
+
+        if state == "ARRIVING":
+            return f"Linka {route_short_name} sa blíži k zastávke {stop_name}."
+
+        if state == "AT_STOP":
+            return f"Linka {route_short_name} je na zastávke {stop_name}."
+
+        if state == "DEPARTING":
+            return f"Linka {route_short_name} odchádza zo zastávky {stop_name}."
+
+        return None
