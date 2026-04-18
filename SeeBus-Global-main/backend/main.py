94f3ed9d95d2065163ac4122b45172ff32a5dc62
richardpizem-ctrl <richardpizem@gmail.com>
2026-04-18 08:37:59 +0200

    Update main.py

--- SeeBus-Global-main/backend/main.py
+++ SeeBus-Global-main/backend/main.py
@@ -6,9 +6,19 @@ from backend.api.routes import get_status
 # API endpointy
 from backend.api.events_endpoint import router as events_router
 from backend.api.stream_endpoint import router as stream_router
+from backend.api.language_endpoint import router as language_router
+
+# GTFS dáta
+from backend.gtfs.loader import stops, stop_times
+
+# Dispatcher
+from backend.services.event_dispatcher import EventDispatcher
 
 
 app = FastAPI()
+
+# Globálny dispatcher
+dispatcher = EventDispatcher(stops, stop_times)
 
 
 @app.get("/status")
@@ -19,3 +29,4 @@ def status():
 # Registrácia API routerov
 app.include_router(events_router)
 app.include_router(stream_router)
+app.include_router(language_router)
